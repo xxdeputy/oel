07491559bbbb59becd8ab41086a187dfcd9c4d77
Mathias <43956573+xxdeputy@users.noreply.github.com>
2023-12-10 12:08:23 +0100

    Update hent_oel.py

--- hent_oel.py
+++ hent_oel.py
@@ -16,11 +16,11 @@ urllib.request.urlretrieve("https://untappd.com/user/MathiasB91/beers", "untappd
 
 
 # Lokal fil
-# html_file_path = "C:/Users/Mathias/Documents/Dokumenter/113 Brewing/untappd.html"
+html_file_path = "C:/Users/Mathias/Documents/Dokumenter/113 Brewing/untappd.html"
 
 
 # Your input string containing "alt=NAME" instances
-html_file_path = "C:/Users/Mathias/untappd.html"
+#html_file_path = "C:/Users/Mathias/untappd.html"
 
 
 
@@ -31,11 +31,11 @@ with open(html_file_path, 'r', encoding='utf-8') as file:
 # Parse the HTML content using BeautifulSoup
 soup = BeautifulSoup(html_content, 'html.parser')
 
-# Ølnavn
+# Beer
 name_elements = soup.select('.name:not(.sidebar .name)')
 names = [element.get_text(strip=True) for element in name_elements]
 
-# Bryggeri
+# Brewery
 brewery_elements = soup.find_all(class_='brewery')
 breweries = [element.get_text(strip=True) for element in brewery_elements]
 
@@ -55,7 +55,6 @@ for style in styles:
     Type_list.append(Type)
     Undertype_list.append(Undertype)
 
-
 # ABV
 abv_elements = soup.find_all(class_='abv')
 abv_values = []
@@ -68,18 +67,38 @@ for element in abv_elements:
         abv_value = 0
     abv_values.append(abv_value)
 
-
 # IBU
 ibu_elements = soup.find_all(class_='ibu')
 ibu_values = [float(re.search(r'\d+', element.get_text(strip=True)).group()) if re.search(r'\d+', element.get_text(strip=True)) else 0 for element in ibu_elements]
 
+
+#Rating, både min og global
+# Find all elements with class 'ratings'
+rating_elements = soup.find_all(class_='ratings')
+ratings = [element.get_text(strip=True) for element in rating_elements]
+min_ratings2 = [rating[:21] for rating in ratings]
+min_ratings1 = [re.sub(r'[^0-9.-]', '', rating) for rating in min_ratings2]
+My_Rating = [float(rating) if rating else 0 for rating in min_ratings1]
+
+global_rating2 = [rating[29:54] for rating in ratings]
+# Use regular expressions to keep only numeric characters
+global_rating1 = [re.sub(r'[^0-9.-]', '', rating) for rating in global_rating2]
+# Convert to numeric values
+Global_Rating = [float(rating) if rating else 0 for rating in global_rating1]
+
+
 # Date
-date_elements = soup.find_all(class_='date-time')
-date_values = [element.get_text(strip=True) for element in date_elements]
-unique_date_values = list(set(date_values))
+# Find all elements with class 'date'
+date_elements = soup.find_all(class_='date')
+# Extract the date values using BeautifulSoup
+dates = [element.get_text(strip=True) for element in date_elements]
+# Keep only elements containing 'Recent' in the list
+Date_time = [''.join(char for char in date if char.isdigit() or char == '/') for date in dates if 'Recent' in date]
+
+
 
 # Create a DataFrame with the names and breweries
-df = pd.DataFrame({'Øl': names, 'Bryggeri': breweries, 'Type': Type_list, 'Undertype': Undertype_list, 'ABV': abv_values, 'IBU': ibu_values})
+df = pd.DataFrame({'Beer': names, 'Brewery': breweries, 'Type': Type_list, 'Subtype': Undertype_list, 'ABV': abv_values, 'IBU': ibu_values, 'My_Rating': My_Rating, 'Global_Rating': Global_Rating, 'Date_time':Date_time})
 
 # Display the DataFrame
 print(df)
